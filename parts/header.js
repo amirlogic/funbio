e3bd78950814b25672b5cbda4ebc9aa6eddef8bc
Amir Hachaichi <admin@synlaunch.com>
2022-08-17 15:04:31 +0200

    some css changes

--- parts/header.js
+++ parts/header.js
@@ -4,15 +4,15 @@ export default function Header() {
     return (
       <div className="flex flex-wrap items-stretch h-20 w-full pt-4 px-6">
 
-                <div className="w-2/5 sm:w-full h-14 text-xl">
+                <div className="w-2/5 md:w-full h-14 text-xl">
                     
                     Functional Biology
                     
                 </div>
 
-                <div className="w-1/5 h-14 sm:hidden"></div>
+                <div className="w-1/5 h-14 md:hidden"></div>
 
-                <div className="w-2/5 h-14"></div>
+                <div className="w-2/5 h-14 md:hidden"></div>
 
       </div>
     )
